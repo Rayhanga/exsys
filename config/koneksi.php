6de28030a0630959d9a45d1cb09e8ba1485194a5
alfarrel95 <alvinnuradnan@protonmail.com>
2023-01-11 01:25:34 +0700

    Your commit message

---
+++ config/koneksi.php
@@ -0,0 +1,20 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "spkayam";
+
+   $conn = mysqli_connect($server, $username, $password, $database);
+ /*   if (mysqli_connect_errno()) {
+    echo "Koneksi gagal: " . mysqli_connect_error();
+    exit();
+  } else {
+      echo "Koneksi success";
+  } */
+ /* mysql_connect($server,$username,$password) or die("Koneksi gagal");
+mysql_select_db($database) or die("Maaf, Database tidak bisa dibuka");
+*/
+?> 
+
+
+
